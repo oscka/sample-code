ba52708e7176affeaa7745d1cde60e9d1e5b0e46
jinseul <evelyn.park@osckorea.com>
2023-04-13 11:09:58 +0900

    first commit

---
+++ spring-cache/src/main/java/com/sample/cache/Application.java
@@ -0,0 +1,13 @@
+package com.sample.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCacheApplication.class, args);
+	}
+
+}
